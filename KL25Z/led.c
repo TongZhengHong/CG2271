e53c9e93a3d01ec0759d8df1459245b9425e9de3
TongZhengHong <zhtong315@gmail.com>
2023-10-09 21:36:20 +0800

    Remove project (CMSIS 5.9.0)

--- KL25Z/led.c
+++
@@ -1,5 +0,0 @@
-#include "MKL25Z4.h"                    // Device header
-
-void led_control(void *argument) {
-	
-}
--- KL25Z/motors.c
+++
@@ -1,8 +0,0 @@
-#include "MKL25Z4.h"                    // Device header
-
-void motor_control(void *argument) {
-	// Checks current state of program and set the corresponding 
-	// PWM waveform to the corresponding motor pins
-}
-
-
