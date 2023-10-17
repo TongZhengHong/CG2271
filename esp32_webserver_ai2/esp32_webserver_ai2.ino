ed426d006e1d93eeb7b1f25c03b2a97edbd5b6c7
TongZhengHong <zhtong315@gmail.com>
2023-10-17 10:53:24 +0800

    Update ESP code to send new motor command

--- esp32_webserver_ai2/esp32_webserver_ai2.ino
+++ esp32_webserver_ai2/esp32_webserver_ai2.ino
@@ -1,12 +1,15 @@
 // Load Wi-Fi library
+#include <stdlib.h>
 #include <WiFi.h>
 
 #define RXD2 16
 #define TXD2 17
 
+#define OFF_COMMAND 0b10001000
+
 // Replace with your network credentials
-const char* ssid = "mc";
-const char* password = "123mc123";
+const char *ssid = "mc";
+const char *password = "123mc123";
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -26,7 +29,7 @@ unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
-int wait30 = 30000;  // time to reconnect when connection is lost.
+int wait30 = 30000; // time to reconnect when connection is lost.
 
 // // This is your Static IP
 // IPAddress local_IP(192, 168, 131, 50);
@@ -36,7 +39,8 @@ int wait30 = 30000;  // time to reconnect when connection is lost.
 // IPAddress primaryDNS(8, 8, 8, 8);
 // IPAddress secondaryDNS(8, 8, 4, 4);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
   // Initialize the output variables as outputs
@@ -44,17 +48,18 @@ void setup() {
   // Set outputs to LOW
   digitalWrite(output26, LOW);
 
-  //Configure Static IP
-  //if(!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
+  // Configure Static IP
+  // if(!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
   //{
-  //  Serial.println("Static IP failed to configure");
-  //}
+  //   Serial.println("Static IP failed to configure");
+  // }
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -67,10 +72,12 @@ void setup() {
   server.begin();
 }
 
-void loop() {
+void loop()
+{
 
   // If disconnected, try to reconnect every 30 seconds.
-  if ((WiFi.status() != WL_CONNECTED) && (millis() > wait30)) {
+  if ((WiFi.status() != WL_CONNECTED) && (millis() > wait30))
+  {
     Serial.println("Trying to reconnect WiFi...");
     WiFi.disconnect();
     WiFi.begin(ssid, password);
@@ -78,7 +85,8 @@ void loop() {
   }
   // Check if a client has connected..
   WiFiClient client = server.available();
-  if (!client) {
+  if (!client)
+  {
     return;
   }
 
@@ -93,40 +101,58 @@ void loop() {
   String req = client.readStringUntil('\r');
   Serial.println(req);
 
-  // Make the client's request.
-  if (req.indexOf("status") != -1) {
-    response = "WiFi Connected: " + ip_address;
+  // Check for off bit pattern and send OFF command
+  if (req.indexOf("off") != -1)
+  {
+    Serial2.write(OFF_COMMAND);
+    return;
   }
-  if (req.indexOf("onRed") != -1) {
-    digitalWrite(output26, HIGH);
-    response = "RED LED ON";
-    Serial2.write(0x31);
+
+  String motor_ident = "motor=";
+  int index = req.indexOf(motor_ident);
+  if (index == -1)
+  {
+    return; // Motor command NOT found, abort
   }
-  if (req.indexOf("offRed") != -1) {
-    digitalWrite(output26, LOW);
-    response = "RED LED OFF";
-    Serial2.write(0x30);
-  }
-  if (req.indexOf("onGreen") != -1) {
-    digitalWrite(output26, HIGH);
-    response = "GREEN LED ON";
-    Serial2.write(0x33);
-  }
-  if (req.indexOf("offGreen") != -1) {
-    digitalWrite(output26, LOW);
-    response = "GREEN LED OFF";
-    Serial2.write(0x32);
-  }
-  if (req.indexOf("onBlue") != -1) {
-    digitalWrite(output26, HIGH);
-    response = "BLUE LED ON";
-    Serial2.write(0x35);
-  }
-  if (req.indexOf("offBlue") != -1) {
-    digitalWrite(output26, LOW);
-    response = "BLUE LED OFF";
-    Serial2.write(0x34);
-  }
+
+  String command = req.substring(index + motor_ident.length());
+  uint8_t command_value = strtol(command, NULL, 2);
+  Serial2.write(command_value);
+
+  // if (req.indexOf("status") != -1) {
+  //   response = "WiFi Connected: " + ip_address;
+  // }
+  // if (req.indexOf("onRed") != -1) {
+  //   digitalWrite(output26, HIGH);
+  //   response = "RED LED ON";
+  //   Serial2.write(0x31);
+  // }
+  // if (req.indexOf("offRed") != -1) {
+  //   digitalWrite(output26, LOW);
+  //   response = "RED LED OFF";
+  //   Serial2.write(0x30);
+  // }
+  // if (req.indexOf("onGreen") != -1) {
+  //   digitalWrite(output26, HIGH);
+  //   response = "GREEN LED ON";
+  //   Serial2.write(0x33);
+  // }
+  // if (req.indexOf("offGreen") != -1) {
+  //   digitalWrite(output26, LOW);
+  //   response = "GREEN LED OFF";
+  //   Serial2.write(0x32);
+  // }
+  // if (req.indexOf("onBlue") != -1) {
+  //   digitalWrite(output26, HIGH);
+  //   response = "BLUE LED ON";
+  //   Serial2.write(0x35);
+  // }
+  // if (req.indexOf("offBlue") != -1) {
+  //   digitalWrite(output26, LOW);
+  //   response = "BLUE LED OFF";
+  //   Serial2.write(0x34);
+  // }
+
   /*
        if (req.indexOf("on12") != -1) {digitalWrite(LED12, HIGH); estado = "LED12 ON";}
        if (req.indexOf("off12") != -1){digitalWrite(LED12, LOW); estado = "LED12 OFF";}
@@ -138,11 +164,10 @@ void loop() {
            if (digitalRead(LED14) == HIGH) {estado = estado + "LED14 ON";} else {estado = estado + "LED14 OFF";}
            }*/
 
-
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("");
-  client.println(response);  //  Return status.
+  client.println(response); //  Return status.
 
   client.flush();
   client.stop();
